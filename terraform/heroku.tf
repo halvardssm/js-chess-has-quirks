7e5475b2bb7912e77d922a6d4f75b1b68bdf0daa
halvardssm <halvard@simplyundoable.com>
2019-12-15 13:28:20 +0100

    changes to tf

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -15,7 +15,7 @@ provider "heroku" {
 resource "heroku_app" "api" {
   name   = "g2-cse1500-api"
   region = "eu"
-  acm    = true
+
 }
 
 resource "heroku_app_release" "api" {
@@ -28,41 +28,40 @@ resource "heroku_formation" "api" {
   type       = "web"
   quantity   = 1
   size       = "free"
-  depends_on = ["heroku_app_release.api"]
+  depends_on = [heroku_app_release.api]
 }
 
 resource "heroku_build" "api" {
-  app        = heroku_app.api.i
-  buildpacks = ["heroku/nodejs"]
+  app = heroku_app.api.id
   source = {
-    path = "./src"
+    path = "./"
   }
 }
 
 ### UI
 
-resource "heroku_app" "app" {
-  name   = "g2-cse1500-app"
-  region = "us"
-  acm    = true
+# resource "heroku_app" "app" {
+#   name   = "g2-cse1500-app"
+#   region = "us"
+#   acm    = true
 
-  config_vars = {
-    API_URL = "https://g2-cse1500-app.herokuapp.com"
-  }
-}
+#   config_vars = {
+#     API_URL = "https://g2-cse1500-app.herokuapp.com"
+#   }
+# }
 
-resource "heroku_app_release" "app" {
-  app     = heroku_app.app.id
-  slug_id = "g2-cse1500-app"
-}
+# resource "heroku_app_release" "app" {
+#   app     = heroku_app.app.id
+#   slug_id = "g2-cse1500-app"
+# }
 
-resource "heroku_formation" "app" {
-  app        = heroku_app.app.id
-  type       = "web"
-  quantity   = 1
-  size       = "free"
-  depends_on = ["heroku_app_release.app"]
-}
+# resource "heroku_formation" "app" {
+#   app        = heroku_app.app.id
+#   type       = "web"
+#   quantity   = 1
+#   size       = "free"
+#   depends_on = [heroku_app_release.app]
+# }
 # Add if necessarry
 # resource "heroku_domain" "app" {
 #   app      = heroku_app.app.name
