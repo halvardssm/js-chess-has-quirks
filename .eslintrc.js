910c60e37ded2d7c33557bacbc86b58a2d313e96
halvardssm <halvard@simplyundoable.com>
2019-12-18 13:56:21 +0100

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
 module.exports = {
-  env: {
-    node: true,
-    es6: true,
-    "jest": true
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: 2015,
-    sourceType: "module"
-  },
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "no-console": 0
-  }
+	env: {
+		node: true,
+		es6: true,
+		"jest": true
+	},
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: 2015,
+		sourceType: "module"
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'key-spacing': ['error', { afterColon: true, mode: 'strict' }],
+		'eol-last': ['error', 'always'],
+		'no-unused-vars': ['warn'],
+		'no-mixed-spaces-and-tabs': ['error'],
+		'object-curly-spacing': ["error", "always", { "arraysInObjects": false, "objectsInObjects": false }],
+		'array-bracket-spacing': ["error", "never", { "objectsInArrays": false, "singleValue": false, "arraysInArrays": false }],
+		'computed-property-spacing': ["error", "never", { "enforceForClassMembers": true }],
+		'space-infix-ops': "error",
+		'semi-spacing': "error"
+	}
 };
