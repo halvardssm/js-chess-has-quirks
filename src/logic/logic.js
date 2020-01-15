428b5347bc948b04138ad05d32375cc346ca7e75
Druue <tileyratkins@gmail.com>
2020-01-15 14:25:21 +0100

    logic

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -3,59 +3,76 @@ const Game = require('./Game')
 const messages = require('../../public/shared-js/messages')
 const { TYPES, Position, ChessPiece } = require('../../public/shared-js/structs')
 
-const pawnLogic = (start, end) => { }
+/**
+ * @param {ChessPiece[][]} board
+ * @param {string} colour
+ * @param {Position} start 
+ * @param {Position} end 
+ */
+const pawnLogic = (board, colour, start, end) => {
+	let direction = (colour === 'BLACK') ? -1 : 1
 
-const rookLogic = (start, end) => { }
+	if (start.x === end.x) {
+		return true
+	}
+}
 
-const knightLogic = (start, end) => { }
+const rookLogic = (board, start, end) => Math.abs(start.x - end.x) === 0 || Math.abs(start.y - end.y) === 0 ? rookLogicHelper(board, start, end) : false
 
-const bishopLogic = (start, end) => Math.abs(start.x - end.x) !== Math.abs(start.y - end.y) ? false : bishopHelper(start, end)
+const rookLogicHelper = (board, position, end) => {
+
+}
+
+const knightLogic = (board, start, end) => { }
+
+const bishopLogic = (board, start, end) => Math.abs(start.x - end.x) === Math.abs(start.y - end.y) ? false : bishopHelper(board, start, end)
 
 const bishopHelper = (curr, end) => {
 
 }
 
-const queenLogic = (start, end) => { }
+const queenLogic = (board, start, end) => { }
 
-const kingLogic = (start, end) => { }
+const kingLogic = (board, start, end) => { }
 
 /**
- * 
+ * @param {ChessPiece[][]} board
  * @param {ChessPiece} piece 
  * @param {Position} end 
  * @return {boolean}
  */
-const validateMove = (piece, end) => {
+const validateMove = (board, piece, end) => {
 	let start = new Position(piece.position.x, piece.position.y)
 
+
 	switch (piece.type) {
-	case TYPES.pawn:
-		pawnLogic(start, end) ? piece.move(end) : console.log('Invalid move!')
-		break
+		case TYPES.pawn:
+			pawnLogic(board, start, end) ? piece.move(end) : console.log('Invalid move!')
+			break
 
-	case TYPES.rook:
-		rookLogic(start, end) ? piece.move(end) : console.log('Invalid Move!')
-		break
+		case TYPES.rook:
+			rookLogic(board, start, end) ? piece.move(end) : console.log('Invalid Move!')
+			break
 
-	case TYPES.knight:
-		knightLogic(start, end) ? piece.move(end) : console.log('Invalid move!')
-		break
+		case TYPES.knight:
+			knightLogic(board, start, end) ? piece.move(end) : console.log('Invalid move!')
+			break
 
-	case TYPES.bishop:
-		bishopLogic(start, end) ? piece.move(end) : console.log('Invalid move!')
-		break
+		case TYPES.bishop:
+			bishopLogic(board, start, end) ? piece.move(end) : console.log('Invalid move!')
+			break
 
-	case TYPES.queen:
-		queenLogic(start, end) ? piece.move(end) : console.log('Invalid move!')
-		break
+		case TYPES.queen:
+			queenLogic(board, start, end) ? piece.move(end) : console.log('Invalid move!')
+			break
 
-	case TYPES.king:
-		kingLogic(start, end) ? piece.move(end) : console.log('Invalid move!')
-		break
+		case TYPES.king:
+			kingLogic(board, start, end) ? piece.move(end) : console.log('Invalid move!')
+			break
 
-	default:
-		console.log('Invalid piece given')
-		break
+		default:
+			console.log('Invalid piece given')
+			break
 	}
 }
 
@@ -66,18 +83,18 @@ const playerTurn = (gameObject, message, connection) => {
 
 	let isCurrentPlayer = game.playerW == connection ? true : false
 
-	switch(message.type){
-	case T_GAME_START:
+	switch (message.type) {
+		case T_GAME_START:
 
 
-		break
-	case T_MOVE_PIECE:
-			
+			break
+		case T_MOVE_PIECE:
+
 	}
 }
 
 module.exports = { playerTurn, validateMove }
-	
+
 // 	if (isPlayerW) {
 // 		/*
 //    * player W cannot do a lot, just send the target word;
@@ -108,4 +125,3 @@ module.exports = { playerTurn, validateMove }
 // 			game.gamesCompleted++
 // 		}
 // 	}
-
