43283d77f724f113189a539d62a4b5caf8a21009
Druue <tileyratkins@gmail.com>
2020-01-19 15:31:03 +0100

    Removed debugging from initValidMoves, added movePiece(..) to GameState. GameState has been JSDOC-ified

--- src/logic/GameState.js
+++ src/logic/GameState.js
@@ -1,30 +1,50 @@
-import { COLOUR, ChessPiece, generateEmptyBoardArray, Position, getDirection } from '../../public/lib/index.js'
+import { COLOUR, ChessPiece, generateEmptyBoardArray, Position } from '../../public/lib/index.js'
 import { getValidMoves } from './index.js'
 
+/**
+ * Class representing the GameState
+ * @property {number} gameId
+ * @property {WebSocket} playerW
+ * @property {WebSocket} playerB
+ * @property {ChessPiece[][]} gameBoard
+ * @property {WebSocket} winner
+ */
 export class GameState {
 
-	/**
-	 * @param {number} gameId 
-	 */
+	/** @param {number} gameId */
 	constructor(gameId) {
 		this.id = gameId
+
+		/** @type {WebSocket} */
 		this.playerW = null
+
+		/** @type {WebSocket} */
 		this.playerB = null
+
 		/** @type ChessPiece[][] */
 		this.gameBoard = generateEmptyBoardArray()
+
+		/** @type {WebSocket} */
 		this.winner = null
 
 		this.initValidMoves()
 	}
 
+	/**
+	 * Checks whether the current game has two players
+	 * @returns {boolean}
+	 */
 	hasTwoConnectedPlayers() {
 		return this.playerB !== null
 	}
 
+	/**
+	 * If the current game is not full, adds the given player to the game
+	 * @param {WebSocket} player 
+	 * @throws {Error} Game already full
+	 */
 	addPlayer(player) {
-		if (this.playerW && this.playerB) {
-			return new Error('Invalid call to addPlayer, Game full')
-		}
+		if (this.playerW && this.playerB) return new Error('Invalid call to addPlayer, Game full')
 
 		if (this.playerW === null) {
 			this.playerW = player
@@ -35,28 +55,37 @@ export class GameState {
 		}
 	}
 
-	movePiece() {
-		//TODO
+	/**
+	 * @param {Position} start Position to move from
+	 * @param {Position} target Position to move to
+	 * @returns {void}
+	 */
+	movePiece(start, target) {
+		let piece = this.gameBoard[start.x][start.y]
+		this.gameBoard[start.x][start.y] = null
+		piece.move(target)
+		this.gameBoard[target.x][target.y] = piece
 	}
 
+	/** @returns {WebSocket} The Winner */
 	getWinner() {
 		return this.winner
 	}
 
+	/** @returns {ChessPiece[][]} The Gameboard array */
 	getBoard() {
 		return this.gameBoard
 	}
 
+	/**
+	 * Initialises the valid moves of each piece on the game board on game start
+	 * @returns {void}
+	 */
 	initValidMoves() {
 		console.log('initValidMoves')
 		this.gameBoard.map(arr => arr
 			.filter(Boolean)
-			.map(cp => {
-				const validMoves = getValidMoves(this.gameBoard, cp)
-				
-				console.log(validMoves)
-				cp.updateValidMoves(validMoves)
-				return cp
-			}))
+			.map(cp => cp.updateValidMoves(getValidMoves(this.gameBoard, cp)))
+		)
 	}
 }
